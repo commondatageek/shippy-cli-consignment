bef2f7f8e17341b73c78a397b4042ca38a3f5945
aaron-johnson <aaron-johnson@pluralsight.com>
2020-09-09 13:13:14 -0600

    Add GetConsignments to client

--- main.go
+++ main.go
@@ -54,4 +54,13 @@ func main() {
 		log.Fatalf("Could not greet: %v", err)
 	}
 	log.Printf("Created: %t", r.Created)
+
+	// get a list of all consignments
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
 }
